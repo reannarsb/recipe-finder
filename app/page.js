661217e3161f73c2be424d158e215d54a068b71d
sizerinaa <118241682+sizerinaa@users.noreply.github.com>
2024-08-04 14:09:06 -0600

    Updated page.js

--- app/page.js
+++ app/page.js
@@ -6,3 +6,18 @@ export default function Page() {
         </main>
     );
 }
+
+import Image from "./image";
+import SingleRecipe from "./single-recipe";
+ 
+export default function Page() {
+    return (
+        <main className="p-8">
+            <header className="text-center mb-8">
+                <h1 className="text-4xl font-bold">Delightful</h1>
+                <h2 className="text-2xl">Discover delicious recipes</h2>
+            </header>
+            <Image />
+        </main>
+    );
+}
